c02d1f70961067b710ce80ff96b347dc07c62e0d
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2023-06-04 10:06:46 -0400

    Edit seed and schema for events and users

--- controllers/userEventsController.js
+++ controllers/userEventsController.js
@@ -3,6 +3,7 @@ const usersEvents = express.Router();
 const {
   getAllUsersEvents,
   getFirstFourUsers,
+  getTotalRSVPS,
 } = require("../queries/usersEvents");
 
 // FIRST FOUR USERS
@@ -10,6 +11,15 @@ usersEvents.get("/firstfour", async (req, res) => {
   const firstFourUsers = await getFirstFourUsers();
   if (Array.isArray(firstFourUsers)) {
     res.status(200).json(firstFourUsers);
+  } else {
+    res.status(400).json({ error: "Server Error" });
+  }
+});
+
+usersEvents.get("/totalrsvps", async (req, res) => {
+  const totalRSVPS = await getTotalRSVPS();
+  if (Array.isArray(totalRSVPS)) {
+    res.status(200).json(totalRSVPS);
   } else {
     res.status(400).json({ error: "Server Error" });
   }
