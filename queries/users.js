eafe266d11363f56f7e92760c93842be57573da3
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2023-06-12 18:34:10 -0400

    Fix user edit queries

--- queries/users.js
+++ queries/users.js
@@ -10,11 +10,13 @@ const getAllUsers = async () => {
 };
 
 // ONE USER
-const getUser = async (stytch_id) => {
+const getUser = async (idParam) => {
   try {
     const user = await db.oneOrNone(
-      "SELECT * FROM users WHERE stytch_id=$1",
-      stytch_id
+      `SELECT * FROM users WHERE ${
+        idParam.includes("user") ? "stytch_id=$1" : "id=$1"
+      }`,
+      idParam
     );
     return user;
   } catch (error) {
@@ -59,11 +61,11 @@ const createUser = async (user) => {
 };
 
 // DELETE USER
-const deleteUser = async (stytch_id) => {
+const deleteUser = async (id) => {
   try {
     const deletedUser = await db.one(
-      "DELETE FROM users WHERE stytch_id=$1 RETURNING *",
-      stytch_id
+      "DELETE FROM users WHERE id=$1 RETURNING *",
+      id
     );
     return deletedUser;
   } catch (error) {
@@ -72,7 +74,7 @@ const deleteUser = async (stytch_id) => {
 };
 
 // UPDATE USER
-const updateUser = async (stytch_idParam, user) => {
+const updateUser = async (idParam, user) => {
   const {
     first_name,
     middle_name,
@@ -81,11 +83,13 @@ const updateUser = async (stytch_idParam, user) => {
     about_me,
     interests,
     intra_extraversion,
-    phone_number
+    phone_number,
   } = user;
   try {
     const updatedUser = await db.one(
-      "UPDATE users SET first_name=$1, middle_name=$2, last_name=$3, username=$4, about_me=$5, interests=$6, intra_extraversion=$7, phone_number=$8 WHERE stytch_id=$9 RETURNING *",
+      `UPDATE users SET first_name=$1, middle_name=$2, last_name=$3, username=$4, about_me=$5, interests=$6, intra_extraversion=$7, phone_number=$8 WHERE ${
+        idParam.includes("user") ? "stytch_id=$9" : "id=$9"
+      } RETURNING *`,
       [
         first_name,
         middle_name,
@@ -95,7 +99,7 @@ const updateUser = async (stytch_idParam, user) => {
         interests,
         intra_extraversion,
         phone_number,
-        stytch_idParam,
+        idParam,
       ]
     );
 
@@ -108,10 +112,13 @@ const updateUser = async (stytch_idParam, user) => {
 
 //UPDATE PROFILE PIC FOR USER
 const updateUserPicture = async (id, path) => {
-  const updateUserPicture = await db.one("UPDATE users SET profile_pic=$1 WHERE stytch_id=$2 RETURNING *", [path, id]);
+  const updateUserPicture = await db.one(
+    "UPDATE users SET profile_pic=$1 WHERE id=$2 RETURNING *",
+    [path, id]
+  );
 
-  return updateUserPicture
-}
+  return updateUserPicture;
+};
 
 module.exports = {
   getAllUsers,
@@ -119,5 +126,5 @@ module.exports = {
   createUser,
   deleteUser,
   updateUser,
-  updateUserPicture
+  updateUserPicture,
 };
